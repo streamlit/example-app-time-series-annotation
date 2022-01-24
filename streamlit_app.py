47bf5522bfc4dc191e1374bc84122720af2adcd6
arnaudmiribel <arnaudmiribel@gmail.com>
2022-01-24 13:20:50 +0100

    Add page info

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import streamlit as st
 from vega_datasets import data
 
+st.set_page_config(title="Time series annotations", page_icon="⬇", layout="centered")
+
 
 def get_chart(data):
     hover = alt.selection_single(
